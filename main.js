a13016ebc906f07c66467a30e93f43f5ccaa27b7
Christian Fleschhut <weltrekordhalter@gmail.com>
2019-12-03 19:41:38 +0100

    Day 5

--- main.js
+++ main.js
@@ -4,11 +4,22 @@ new Vue({
     greeting: 'Hello World!',
     user: 'Hassan Djirdeh',
     city: 'Toronto',
+    cityImage: 'https://bit.ly/2SKJPqJ',
+    numbers: [
+      { id: 1, value: 1 },
+      { id: 2, value: 10 },
+      { id: 3, value: 100 },
+      { id: 4, value: 1000 },
+      { id: 5, value: 10000 },
+    ],
   },
   methods: {
     changeGreeting() {
       this.greeting =
         this.greeting === 'Hello World!' ? 'What is up!' : 'Hello World!';
+    },
+    shuffle() {
+      this.numbers = _.shuffle(this.numbers);
     },
   },
 });
