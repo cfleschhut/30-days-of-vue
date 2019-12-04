a6e3b11ac4102d74bcf733016287bbdbd97b97a5
Christian Fleschhut <weltrekordhalter@gmail.com>
2019-12-04 16:12:20 +0100

    Day 12

--- main.js
+++ main.js
@@ -1,27 +1,88 @@
+Vue.component('tweet-content', {
+  props: ['tweet'],
+  template: `
+  <div class="media-content">
+    <div class="content">
+      <p>
+        <strong>{{tweet.name}}</strong>
+        <small>{{tweet.handle}}</small>
+        <br />
+        {{tweet.tweet}}
+      </p>
+    </div>
+    <div class="level-left">
+      <a class="level-item">
+        <span class="icon is-small">
+          <i class="fas fa-heart"></i>
+        </span>
+        <span class="likes">
+          {{tweet.likes}}
+        </span>
+      </a>
+    </div>
+  </div>
+  `,
+});
+
+Vue.component('tweet-component', {
+  props: {
+    tweet: {
+      type: Object,
+      required: true,
+    },
+  },
+  template: `
+  <div class="tweet">
+    <div class="box">
+      <article class="media">
+        <div class="media-left">
+          <figure class="image is-64x64">
+            <img :src="tweet.img" />
+          </figure>
+        </div>
+        <tweet-content :tweet="tweet" />
+      </article>
+    </div>
+    <div class="control has-icons-left">
+      <input class="input is-small" placeholder="Tweet your reply..." />
+      <span class="icon is-small is-left">
+        <i class="fas fa-envelope"></i>
+      </span>
+    </div>
+  </div>
+  `,
+});
+
+const tweets = [
+  {
+    id: 1,
+    name: 'James',
+    handle: '@jokerjames',
+    img: './images/james.png',
+    tweet: "If you don't succeed, dust yourself off...",
+    likes: 10,
+  },
+  {
+    id: 2,
+    name: 'Fatima',
+    handle: '@fantasticfatima',
+    img: './images/fatima.png',
+    tweet: 'Better late than never but never late...',
+    likes: 12,
+  },
+  {
+    id: 3,
+    name: 'Xin',
+    handle: '@xeroxin',
+    img: './images/xin.png',
+    tweet: 'Beauty in the struggle...',
+    likes: 18,
+  },
+];
+
 new Vue({
   el: '#app',
   data: {
-    name: 'Loading…',
-    email: 'Loading…',
-    company: {
-      name: 'Loading…',
-      catchPhrase: 'Loading…',
-    },
-  },
-  created() {
-    axios.get('http://jsonplaceholder.typicode.com/users').then(({ data }) => {
-      const randomUser = data[Math.floor(Math.random() * data.length)];
-
-      this.name = randomUser.name;
-      this.email = randomUser.email;
-      this.company.name = randomUser.company.name;
-      this.company.catchPhrase = randomUser.company.catchPhrase;
-    });
-  },
-  mounted() {
-    console.log(this.$el);
-  },
-  updated() {
-    console.log(this.$el);
+    tweets,
   },
 });
