8f156dc8f3e6c5853233e06381eb1af0c6cd0279
Christian Fleschhut <weltrekordhalter@gmail.com>
2019-12-21 00:24:59 +0100

    Day 28 (Vue Test Utils)

--- default_project/tests/unit/Todos.spec.js
+++ default_project/tests/unit/Todos.spec.js
@@ -1,21 +1,56 @@
-import Vue from 'vue';
+import { shallowMount } from '@vue/test-utils';
 import Todos from '@/components/Todos';
 
 describe('Todos', () => {
-  it('should render correct contents', () => {
-    const Constructor = Vue.extend(Todos);
-    const vm = new Constructor().$mount();
+  let wrapper;
 
-    expect(vm.$el.querySelector('.title').textContent).toBe('Todos');
-    expect(vm.$el.querySelector('.new-todo').placeholder).toBe(
+  beforeEach(() => {
+    wrapper = shallowMount(Todos);
+  });
+
+  it('should render correct contents', () => {
+    expect(wrapper.find('.title').text()).toBe('Todos');
+    expect(wrapper.find('.new-todo').element.placeholder).toBe(
       'What needs to be done?',
     );
   });
 
   it('should set correct default data', () => {
-    const initialData = Todos.data();
+    expect(wrapper.vm.todos).toEqual([]);
+    expect(wrapper.vm.newTodo).toEqual('');
+  });
 
-    expect(initialData.todos).toEqual([]);
-    expect(initialData.newTodo).toEqual('');
+  describe('user populates the text input field', () => {
+    let inputField;
+
+    beforeEach(() => {
+      inputField = wrapper.find('.new-todo');
+
+      inputField.element.value = 'New Todo';
+      inputField.trigger('input');
+    });
+
+    it('should update "newTodo"', () => {
+      expect(wrapper.vm.newTodo).toEqual('New Todo');
+    });
+
+    describe('and presses Enter', () => {
+      it('should add a new todo to "todos"', () => {
+        inputField.trigger('keyup.enter');
+
+        expect(wrapper.vm.todos).toEqual(['New Todo']);
+      });
+    });
+
+    describe('and presses Enter + removes todo', () => {
+      xit('should have the new todo removed', () => {
+        inputField.trigger('keyup.enter');
+
+        const removeIcon = wrapper.find('.destroy');
+        removeIcon.trigger('click');
+
+        expect(wrapper.vm.todos).toEqual([]);
+      });
+    });
   });
 });
